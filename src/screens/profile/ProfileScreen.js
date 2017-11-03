ca3e3e491afed67479b7279f73c010592ad427ca
Thanh Pham <thanhpcc1996@gmail.com>
2017-11-03 08:46:30 +0700

    lalalala 3/11/2017

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -6,6 +6,7 @@ import {
   TouchableHighlight,
   Image
 } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
 import NavBarButton from "../../common/NavBarButton";
 class ProfileScreen extends Component {
@@ -39,28 +40,36 @@ class ProfileScreen extends Component {
             />
           </TouchableHighlight>
           <View style={styles.fullname}>
-            <Text>Hao Nguyen</Text>
+            <Text style={styles.fullnameText}>Hao Nguyen</Text>
           </View>
         </View>
-        <View style={styles.infoConatiner} />
+        <View style={styles.infoConatiner}>
+          <View style={styles.infoHeaderContainer}>
+            <Text style={styles.infoHederText}> Thong tin ca nhan</Text>
+            <TouchableHighlight style={styles.buttonEdit} underlayColor={"#B3E5FC"}>
+              <Ionicons name="md-create" color={'#FFF'} size={20} />
+            </TouchableHighlight>
+          </View>
+        </View>
       </View>
     );
   }
 }
 const styles = StyleSheet.create({
   root: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#FAFAFA"
   },
   avatarContainer: {
     flex: 0.4,
     justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "green"
+    alignItems: "center"
   },
   imageConatiner: {
     width: 160,
     height: 160,
-    borderRadius: 80
+    borderRadius: 80,
+    position: "relative"
   },
   image: {
     width: 160,
@@ -68,14 +77,43 @@ const styles = StyleSheet.create({
     borderRadius: 80
   },
   fullname: {
-    backgroundColor: "red",
+    //backgroundColor: "red",
     position: "absolute",
     bottom: 10,
-    alignSelf: "center"
+    alignSelf: "center",
+    paddingTop: 3
   },
-
+  fullnameText: {
+    color: "#1E88E5",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
   infoConatiner: {
     flex: 0.6
+  },
+  infoHeaderContainer: {
+    height: 60,
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: "#29B6F6"
+  },
+  infoHederText: {
+    marginLeft: 10,
+    color: "#FFF",
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  buttonEdit: {
+    height: 40,
+    width: 40,
+    borderRadius: 20,
+    backgroundColor: "#29B6F6",
+    marginRight: 15,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1.5,
+    borderColor:'#FFF'
   }
 });
 export default ProfileScreen;
